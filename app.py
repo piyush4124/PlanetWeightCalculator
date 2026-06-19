e989fa0a899703cb1723ad6d388e20e18f6337ba
piyush4124 <88713124+piyush4124@users.noreply.github.com>
2026-06-19 13:10:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! My Python project is live 🚀"
+
+if __name__ == "__main__":
+    app.run()
+  
 print('Number	Planet	Relative Gravity\n1	Mercury	0.38\n2	Venus	0.91\n3	Mars	0.38\n4	Jupiter	2.53\n5	Saturn	1.07\n6	Uranus	0.89\n7	Neptune	1.14')
 
 weight = float(input('\nEnter your weight on Earth: '))
